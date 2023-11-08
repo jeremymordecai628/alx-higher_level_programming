158cd22f7d72367f9b3bf45ad95a89bcd03f8ad9
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-11-08 08:00:44 -0800

    huha

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,6 @@
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
-
+    if isinstance(key, str):  # Check if key is a string
+        a_dictionary[key] = value
+        return a_dictionary
+    else:
+        raise ValueError("Key must be a string")
