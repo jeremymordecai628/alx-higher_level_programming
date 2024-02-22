308d3bb459e5a649b2075c7434bdf126aeb43610
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-02-22 10:18:44 -0800

    trail

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Module that lists all states from mySQL database"""
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    # Check if all required command-line arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command-line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object to execute SQL queries
+def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host='localhost',\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
     cursor = db.cursor()
 
-    # Execute the SQL query to select all states and order by id
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows from the result set
+    # Fetch all the rows from the query result
     rows = cursor.fetchall()
 
-    # Display the results as specified
+    # Print the results
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
-    cursor.close()
+    # Close the database connection
     db.close()
 
+# Example usage
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
