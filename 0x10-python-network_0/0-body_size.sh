d926f130b601a544f9dc845496b480a78ecc49f9
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-03-02 06:14:42 -0800

    trial

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,6 @@
-2
-2
+#!/bin/bash
+# Sends a request to a URL and displays the size of the body of the response
+
+URL=$1
+SIZE=$(curl -sI $URL | grep -i Content-Length | awk '{print $2}')
+echo $SIZE
